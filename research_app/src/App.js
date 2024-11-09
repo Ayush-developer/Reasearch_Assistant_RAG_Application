f7f7898019a8f00d6b98d2a62122cc7a9d4f3bfd
Ayush Deb <84736562+Ayush-developer@users.noreply.github.com>
2024-11-10 02:04:43 +0530

    Add files via upload

---
+++ research_app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import QueryForm from './queryform';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>  Reserach Assistant Application</h1>
+            <QueryForm />
+        </div>
+    );
+}
+
+export default App;
